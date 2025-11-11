48cb1c1125f2aaa3d7c6d205884072fa75780e70
akarpovan <akarpovan@gmail.com>
2025-11-11 00:05:31 -0500

    files added

---
+++ week_03/scripts/weather.js
@@ -0,0 +1,34 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.74&lon=6.64&appid=36781a4ce53d1cb48a6a5976d1fc3e33';
+
+//const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.74&lon=6.64&units=imperial&appid=36781a4ce53d1cb48a6a5976d1fc3e33';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data); // testing only
+            displayResults(data); // uncomment when ready
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+}
+
+apiFetch();
